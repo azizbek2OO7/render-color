dfe2cc977ee74911c2a3f45ec8023b9bc5b5c4b7
azizbek2OO7 <malikovazizbek726@gmail.com>
2023-04-27 21:49:43 +0500

    render-color

---
+++ script.js
@@ -0,0 +1,72 @@
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
+var UI = /** @class */ (function () {
+    function UI() {
+        this.symbols = "123456789abcdef";
+        this.wrapper = document.querySelector(".box");
+        this.cardElm = document.querySelector(".card span");
+        this.moreElm = document.querySelector(".more");
+        this.clickElm = document.querySelector(".click span");
+        this.reloadElm = document.querySelector(".reload");
+    }
+    UI.prototype.randColor = function () {
+        var color = "#";
+        for (var i = 0; i < 6; i++) {
+            var idx = Math.floor(Math.random() * this.symbols.length);
+            color += this.symbols[idx];
+        }
+        return color;
+    };
+    UI.prototype.renderCell = function () {
+        var _this = this;
+        for (var i = 0; i < 8; i++) {
+            var cell = document.createElement("div");
+            cell.textContent = this.randColor();
+            cell.style.backgroundColor = cell.textContent;
+            cell.classList.add("cell");
+            this.wrapper.appendChild(cell);
+        }
+        __spreadArray([], this.wrapper.children, true).forEach(function (cell, idx) {
+            cell.addEventListener("click", function () {
+                document.body.style.backgroundColor = cell.textContent;
+                _this.clickElm.textContent = idx + 1;
+            });
+        });
+    };
+    UI.prototype.handlerMore = function () {
+        var _this = this;
+        this.moreElm.addEventListener("click", function () {
+            _this.renderCell();
+            _this.wrapper.style.overflowY = "scroll";
+            _this.cardElm.textContent = +_this.cardElm.textContent + 8;
+        });
+    };
+    UI.prototype.handlerReload = function () {
+        var _this = this;
+        this.reloadElm.addEventListener("click", function () {
+            __spreadArray([], _this.wrapper.children, true).forEach(function (cell) { return cell.remove(); });
+            setTimeout(function () {
+                _this.renderCell();
+            }, 100);
+            _this.cardElm.textContent = "";
+            _this.clickElm.textContent = "";
+            document.body.style.backgroundColor = "#fff";
+            _this.wrapper.style.overflowY = "hidden";
+        });
+    };
+    UI.prototype.init = function () {
+        this.renderCell();
+        this.handlerMore();
+        this.handlerReload();
+    };
+    return UI;
+}());
+var renderColors = new UI();
+renderColors.init();
---
+++ script.ts
@@ -0,0 +1,70 @@
+class UI {
+  symbols = "123456789abcdef";
+
+  wrapper = document.querySelector(".box");
+  cardElm = document.querySelector(".card span");
+  moreElm = document.querySelector(".more");
+  clickElm = document.querySelector(".click span");
+  reloadElm = document.querySelector(".reload");
+
+  randColor() {
+    let color = "#";
+
+    for (let i = 0; i < 6; i++) {
+      const idx = Math.floor(Math.random() * this.symbols.length);
+      color += this.symbols[idx];
+    }
+
+    return color;
+  }
+
+  renderCell() {
+    for (let i = 0; i < 8; i++) {
+      const cell = document.createElement("div");
+
+      cell.textContent = this.randColor();
+      cell.style.backgroundColor = cell.textContent;
+      cell.classList.add("cell");
+
+      this.wrapper.appendChild(cell);
+    }
+
+    [...this.wrapper.children].forEach((cell, idx) => {
+      cell.addEventListener("click", () => {
+        document.body.style.backgroundColor = cell.textContent;
+        this.clickElm.textContent = idx + 1;
+      });
+    });
+  }
+
+  handlerMore() {
+    this.moreElm.addEventListener("click", () => {
+      this.renderCell();
+      this.wrapper.style.overflowY = "scroll";
+      this.cardElm.textContent = +this.cardElm.textContent + 8;
+    });
+  }
+
+  handlerReload() {
+    this.reloadElm.addEventListener("click", () => {
+      [...this.wrapper.children].forEach((cell) => cell.remove());
+      setTimeout(() => {
+        this.renderCell();
+      }, 100);
+      this.cardElm.textContent = "";
+      this.clickElm.textContent = "";
+      document.body.style.backgroundColor = "#fff";
+      this.wrapper.style.overflowY = "hidden";
+    });
+  }
+
+  init() {
+    this.renderCell();
+    this.handlerMore();
+    this.handlerReload();
+  }
+}
+
+const renderColors = new UI();
+
+renderColors.init();
